d3b8a94e34c923b5cb33dd2fa895fc52dcd29c08
v1stra <travis@v1stra.io>
2023-09-14 15:59:29 +0000

    Updated

--- metasmash.py
+++ metasmash.py
@@ -1,36 +1,9 @@
 import sys
 import os
 import magic
-import pyfiglet
-
-result = pyfiglet.figlet_format("MetaSmash")
-print(result)
-
-print('				   		   By Aardwolf Security\n\n')
-
-try:
-    import exiftool
-except ImportError:
-    print("Error: The 'exiftool' module is not installed.")
-    choice = input("Would you like to install it now? (y/n) ")
-    if choice.lower() == "y":
-        os.system("pip install exiftool")
-        import exiftool
-    else:
-        sys.exit()
-
-try:
-    import PIL.ExifTags
-    import PIL.Image
-except ImportError:
-    print("Error: The 'Pillow' module is not installed.")
-    choice = input("Would you like to install it now? (y/n) ")
-    if choice.lower() == "y":
-        os.system("pip install Pillow")
-        import PIL.ExifTags
-        import PIL.Image
-    else:
-        sys.exit()
+import exiftool
+import PIL.ExifTags
+import PIL.Image
 
 SENSITIVE_TAGS = [
     "EXIF:GPSLatitude",
@@ -50,25 +23,26 @@ def extract_metadata(file_path, extract_gps=False):
     """
     Extracts metadata and exif data from a media file.
     """
-    with exiftool.ExifTool() as et:
+    with exiftool.ExifToolHelper() as et:
         if extract_gps:
             metadata = et.get_metadata(file_path, SENSITIVE_TAGS)
         else:
             metadata = et.get_metadata(file_path)
     return metadata
 
-def format_metadata(metadata):
+def format_metadata(metadata_list):
     """
     Formats metadata output to be more human-readable.
     """
     formatted_metadata = ""
-    for key, value in metadata.items():
-        if key in SENSITIVE_TAGS:
+    for metadata in metadata_list:
+        for key, value in metadata.items():
+            # if key in SENSITIVE_TAGS:
             formatted_key = key.replace(":", " ").title()
             formatted_value = str(value).replace("\\n", "\n")
             formatted_metadata += f"{formatted_key}: {formatted_value}\n"
-    if not formatted_metadata:
-        formatted_metadata = "No sensitive metadata found."
+        if not formatted_metadata:
+            formatted_metadata = "No sensitive metadata found."
     return formatted_metadata
 
 def main():
@@ -91,7 +65,7 @@ def main():
     mime = magic.Magic(mime=True)
     file_type = mime.from_file(file_path)
 
-    if "image" in file_type or "pdf" in file_type or "video" in file_type or "audio" in file_type or "ms-office" in file_type:
+    if "image" in file_type or "pdf" in file_type or "video" in file_type or "audio" in file_type or "ms-office" in file_type or "officedocument" in file_type:
         metadata = extract_metadata(file_path, extract_gps)
         formatted_metadata = format_metadata(metadata)
         print(formatted_metadata)
